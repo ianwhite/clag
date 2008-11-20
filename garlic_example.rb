c6af41ae22df7d7d5f9ed2643aba9eb6f0531418
Ian White <ian.w.white@gmail.com>
2008-11-20 22:02:26 +1100

    Declare machinist to be pretty awesome

--- garlic_example.rb
+++ garlic_example.rb
@@ -14,19 +14,21 @@ garlic do
   repo 'rails', :url => 'git://github.com/rails/rails'#, :local => "~/dev/vendor/rails"
   repo 'rspec', :url => 'git://github.com/dchelimsky/rspec'#, :local => "~/dev/vendor/rspec"
   repo 'rspec-rails', :url => 'git://github.com/dchelimsky/rspec-rails'#, :local => "~/dev/vendor/rspec-rails"
+  repo 'cucumber', :url => 'git://github.com/aslakhellesoy/cucumber'#, :local => "~/dev/vendor/cucumber"
+  repo 'factory_girl', :url => 'git://github.com/thoughtbot/factory_girl'#, :local => "~/dev/vendor/factory_girl"
   repo 'clag', :path => '.'
 
   target 'edge'
   target '2.0-stable', :branch => 'origin/2-0-stable'
   target '2.1-stable', :branch => 'origin/2-1-stable'
-  target '2.0.3', :tag => 'v2.0.3'
-  target '2.1.0', :tag => 'v2.1.0'
   
   all_targets do
     prepare do
       plugin 'clag', :clone => true
       plugin 'rspec'
       plugin('rspec-rails') { sh "script/generate rspec -f" }
+      plugin('cucumber') { sh "script/generate cucumber -f" }
+      plugin 'factory_girl'
     end
   
     run do
